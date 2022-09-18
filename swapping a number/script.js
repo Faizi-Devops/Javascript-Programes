6998ac335611b967e23c0b5089ec7f00459b5245
Faizan Ali <devfaizi435@gmail.com>
2022-09-17 22:19:09 -0700

    swapping

---
+++ swapping a number/script.js
@@ -0,0 +1,10 @@
+let a = parseInt(prompt("Please enter a number"));
+let b = parseInt(prompt("Please ente the second number"));
+let c;
+
+console.log("The value  of a is:" + a + "The value of b is:" + b);
+
+c=a;
+a=b;
+b=c;
+alert("The value of a is "+ a +"and the value of b is" + b);
