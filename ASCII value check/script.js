e65ed2f5a33095078e8afda855845be1bf63e0aa
Faizan Ali <devfaizi435@gmail.com>
2022-09-18 20:34:36 -0700

    ascii value

---
+++ ASCII value check/script.js
@@ -0,0 +1,3 @@
+var firstName = prompt("Please enter a alphabet letter");
+var b = firstName.charCodeAt(0);
+alert("The alphabet digit of the letter " + firstName + "is" + b);
---
+++ ASCII value check/script.ts
@@ -0,0 +1,6 @@
+var firstName:any=prompt("Please enter a alphabet letter");
+var b =firstName.charCodeAt(0);
+
+alert("The alphabet digit of the letter " + firstName + "is" + b);
+
+
