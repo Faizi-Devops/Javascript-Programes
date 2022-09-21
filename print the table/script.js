89cd96b6a08e986e07f5e4ac271e40a21a7d6820
Faizan Ali <devfaizi435@gmail.com>
2022-09-20 23:37:19 -0700

    table

---
+++ print the table/script.js
@@ -0,0 +1,5 @@
+let a = parseInt(prompt("Please enter a table number"));
+let b = parseInt(prompt("Please enter the range of the table number"));
+for(let i=1;i<=b;i++){
+    console.log(a + "*" + i + "=" + a*i); 
+}
