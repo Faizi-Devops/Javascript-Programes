2a5c39c6c405fdc0b929755dd0d05c0d605139b5
Faizan Ali <devfaizi435@gmail.com>
2022-09-20 23:35:24 -0700

    find larger number

---
+++ find the largest number/script.js
@@ -0,0 +1,14 @@
+let number1= prompt("Please enter a number");
+let number2=prompt("Please enter a number");
+let number3=prompt("Please enter a number");
+if(number1>number2)
+{
+    
+    alert(number1+"is the larger number");
+}
+else if(number2>number3){
+    alert(number2+ "is the larger number");
+}
+else{
+    alert(number3+ "is the larger number");
+}
