86732970a1342f76c9d759da499d4545804e2dbe
Faizan Ali <devfaizi435@gmail.com>
2022-09-23 02:38:03 -0700

    positive,negative,zero

---
+++ number is positive,negative,zero/script.js
@@ -0,0 +1,12 @@
+let a = parseInt(prompt("Please enter a number"));
+if(a>0)
+{
+    alert("The number is positive");
+}
+else if(a<0)
+{
+    alert("The number is negative");
+}
+else{
+    alert("The number is zero");
+}
